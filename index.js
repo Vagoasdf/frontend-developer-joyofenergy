c26bf8bf586c31e88f35c906469e45fbbabe7353
Bruno Gutierrez Rios <bruno.gutierrez96@gmail.com>
2023-10-03 12:50:35 -0300

    Update index.js
    Adding pure JS reloading button functionality

--- index.js
+++ index.js
@@ -1,5 +1,13 @@
 import { renderChart } from "./chart.js";
 import { getReadings, groupByDay, sortByTime } from "./reading";
 
-const readings = await getReadings();
-renderChart(sortByTime(groupByDay(readings)).slice(-30));
+
+async function reloadReadings() {
+    const readings = await getReadings();
+    renderChart(sortByTime(groupByDay(readings)).slice(-30));
+}
+
+reloadReadings()
+
+const reloadButton = document.querySelector("button");
+reloadButton.addEventListener("click", reloadReadings);
